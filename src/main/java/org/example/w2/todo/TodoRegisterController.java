60b3da0c99b68d3a0b6ee14a0c54a00b6fd7bef5
zk202308A <zk202308A@gmail.com>
2024-07-24 16:32:40 +0900

    dddddd

--- src/main/java/org/example/w2/todo/TodoRegisterController.java
+++ src/main/java/org/example/w2/todo/TodoRegisterController.java
@@ -9,16 +9,19 @@ import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
 
-@WebServlet(value = "/todo/list")
+@WebServlet(value = "/todo/register")
 @Log4j2
-public class TodoListController extends HttpServlet {
-
+public class TodoRegisterController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.info("Todo Register doGET");
 
-        log.info("Todo List GET");
+        req.getRequestDispatcher("/WEB-INF/todo/register.jsp").forward(req, resp);
 
-        req.getRequestDispatcher("/WEB-INF/todo/list.jsp").forward(req, resp);
+    }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.info("Todo Register doPOST");
     }
 }
---
+++ src/main/webapp/WEB-INF/todo/list.jsp
@@ -0,0 +1,7 @@
+
+
+<%@include file="../includes/header.jsp"%>
+
+<h1>Todo List Page</h1>
+
+<%@include file="../includes/footer.jsp"%>
